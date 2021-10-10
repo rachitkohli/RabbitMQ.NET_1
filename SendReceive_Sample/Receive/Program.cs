cb6b5ea19d350a7ae9edde5f6342a4ac54301214
Rachit Kohli <rkohli@symbotic.com>
2021-10-10 17:26:10 -0500

    First example of working with RabbitMQ. This illustrates 2 basic project for Sending message on a Queue & Receiving them

---
+++ SendReceive_Sample/Receive/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using (var connection = factory.CreateConnection()) //Establish connection to Local RabbitMQ node
+            {
+                using (var channel = connection.CreateModel()) //Create channel
+                {
+                    channel.QueueDeclare(queue: "hello",    //this will create a queue, if not present
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ SendReceive_Sample/Send/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using (var connection = factory.CreateConnection()) //Establish connection to Local RabbitMQ node
+            {
+                using (var channel = connection.CreateModel()) //Create channel
+                {
+                    //To send, need to declare a queue, then message can be published to the queue
+                    channel.QueueDeclare(queue: "hello",    //this will create a queue, if not present
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "My first message on Queue Hello";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
