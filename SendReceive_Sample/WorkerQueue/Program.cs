9b9592a8b454f0d12adc0e92eb938d5eea9d0d67
Rachit Kohli <rachitkohli@gmail.com>
2021-10-12 20:12:14 -0500

    Added code for Worker Queue - Distributing the messages

--- SendReceive_Sample/WorkerQueue/Program.cs
+++ SendReceive_Sample/WorkerQueue/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System;
 using System.Text;
 
-namespace Receive
+namespace WorkerQueue
 {
     class Program
     {
@@ -16,34 +16,40 @@ namespace Receive
                 Password = "guest"
             };
 
-            using (var connection = factory.CreateConnection()) //Establish connection to Local RabbitMQ node
+            using (var connection = factory.CreateConnection())
             {
-                using (var channel = connection.CreateModel()) //Create channel
+                using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare(queue: "empQueue",    //this will create a queue, if not present
+                    channel.QueueDeclare(queue: "empQueue",
                         durable: false,
                         exclusive: false,
                         autoDelete: false,
-                        arguments: null);
+                        arguments: null
+                        );
+                    channel.BasicQos(0, 1, false);
 
                     var consumer = new EventingBasicConsumer(channel);
                     Console.WriteLine(" Message Count {0}", channel.MessageCount("empQueue").ToString());
                     consumer.Received += (model, ea) =>
                     {
                         var body = ea.Body.ToArray();
-                        var message = Encoding.UTF8.GetString(body);
                         
+                        var message = Encoding.UTF8.GetString(body);
+                        channel.BasicAck(ea.DeliveryTag, false);    //Acknowledging the message
                         Console.WriteLine(" [x] Received {0}", message);
                     };
-                    
+
                     channel.BasicConsume(queue: "empQueue",
-                        autoAck: true,
+                        autoAck: false,
                         consumer: consumer);
 
                     Console.WriteLine(" Press [enter] to exit.");
                     Console.ReadLine();
+
                 }
             }
         }
+
+        
     }
 }
